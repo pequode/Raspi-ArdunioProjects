3bacfa1e3bc7f1076aaa88e3f7e9ff77e63dd6bd
George Kent-Scheller <georgeks@bu.edu>
2021-08-05 19:43:51 -0400

    added in debug mode

--- PlantControler/waterer/Gardener.cpp
+++ PlantControler/waterer/Gardener.cpp
@@ -15,9 +15,11 @@ void printOutput(string K){
   outfile.close();
   sem_post(&Signal1);//bad to use same sem
 }
+
 void writeVector(){
   sem_wait(&Signal1);//bad to use same sem
-  outfile.open("testFile.txt");
+  if(DEBUG)outfile.open("cont_debug.txt");
+  else outfile.open(ConfPath);
   for(int i =0 ; i<int(Confs.size());i++){
     outfile << Confs[i] << "\n";
   }
@@ -29,11 +31,15 @@ void writeVector(){
   outfile.close();
   sem_post(&Signal1);//bad to use same sem
 }
+
 void ReadTimeingsFile(){
     sem_wait(&Signal1);
-    ifstream input_file(ConfPath);
+    if(!DEBUG) ifstream input_file(ConfPath);
+    else ifstream input_file("cont_debug.txt");
+
     while (!input_file.is_open()){
-      printOutput("ERR:Conf-file is already open sleeping for 5 seconds");
+      if(!DEBUG)printOutput("ERR:Conf-file is already open sleeping for 5 seconds");
+      else cout<< "file open waiting \n"
       sleep(5);
     }
     int number;
@@ -43,80 +49,92 @@ void ReadTimeingsFile(){
     }
     input_file.close();
     sem_post(&Signal1);
-    if(DEBUG) cout<<"Done Read\n\n";
+    if(DEBUG) cout<<"Done Read\n";
 }
 
 int waitTime(int idForSleepSeconds,int idForReset){
    int i;
-   //int Reset = 0;
    for (i=0;i<Confs[idForSleepSeconds];i++){
        delay (1000);
        if (i%10 == 0){
           ReadTimeingsFile();
        }
- /*
-       if((idForSleepSeconds == waterRestartIntervalSec ||
+       if (DEBUG){
+           if((idForSleepSeconds == waterRestartIntervalSec ||
+               idForSleepSeconds == fanRestartIntervalSec   ||
+               idForSleepSeconds == lightRestartIntervalSec) &&  Confs[idForReset] == 1
+             )
+            {
+             cout << "meant to be off but on -- shouldnt be printed without website\n";
+             Confs[idForReset] = 1;
+             writeVector();
+             return 1;
+            }
+           else if(
+             (idForSleepSeconds == waterTimeInd ||
+              idForSleepSeconds == fanTimeInd   ||
+              idForSleepSeconds == lightTimeInd) s
+              &&  Confs[idForReset] == 0
+              ){
+              cout << "meant to be on but off -- shouldnt be printed without website\n";
+              Confs[idForReset] = 0;
+              writeVector();
+              return 0;
+            }
+        }
+
+
+
+   }
+
+    if (DEBUG){
+       if(idForSleepSeconds == waterRestartIntervalSec ||
            idForSleepSeconds == fanRestartIntervalSec   ||
-           idForSleepSeconds == lightRestartIntervalSec) &&  Confs[idForReset] == 1
-         )
-        {
-         cout << "meant to be off but on\n";
-         Confs[idForReset] = 1;
-         writeVector();
-         return 1;
-        }
-       else if(
-         (idForSleepSeconds == waterTimeInd ||
-          idForSleepSeconds == fanTimeInd   ||
-          idForSleepSeconds == lightTimeInd) &&  Confs[idForReset] == 0
-        ){
-          cout << "meant to be on but off\n";
+           idForSleepSeconds == lightRestartIntervalSec){
+          Confs[idForReset] = 1;
+       }else{
           Confs[idForReset] = 0;
-          writeVector();
-          return 0;
-        }
-*/
-
-
-   }
-  
-  /* if(idForSleepSeconds == waterRestartIntervalSec ||
-       idForSleepSeconds == fanRestartIntervalSec   ||
-       idForSleepSeconds == lightRestartIntervalSec){
-      Confs[idForReset] = 1;
-   }else{
-      Confs[idForReset] = 0;
-   }
-   writeVector*/
-   cout << "done Reg\n";
+       }
+       writeVector();
+       cout<<"l99 ish just ran\n";
+    }
+   if (DEBUG) cout << "done Reg\n";
    return -1;
 }
 void* wateringCycle(void* arg){
    while(true){
-    ReadTimeingsFile();
+      ReadTimeingsFile();
 
-    digitalWrite (Water, HIGH);
-    printOutput("Starting Watering: ");
-    waitTime(waterTimeInd,waterRestartTimerInd);
+      if (!DEBUG) digitalWrite (Water, HIGH);
+      if (!DEBUG) printOutput("Starting Watering: ");
+      else cout << "fake Water on\n";
 
-    digitalWrite (Water,  LOW);
-    printOutput("Water Off: ");
-    waitTime(waterRestartIntervalSec,waterRestartTimerInd);
+      waitTime(waterTimeInd,waterRestartTimerInd);
+
+      if (!DEBUG) digitalWrite (Water,  LOW);
+      if (!DEBUG) printOutput("Water Off: ");
+      else cout << "fake Water off\n";
+
+      waitTime(waterRestartIntervalSec,waterRestartTimerInd);
 
    }
     return NULL;
 }
 void* fanCycle(void* arg){
  while(true){
-  ReadTimeingsFile();
+    ReadTimeingsFile();
 
-  digitalWrite (Fans, HIGH);
-  printOutput("Starting Vent: ");
-  waitTime(fanTimeInd,fanRestartTimerInd);
+    if (!DEBUG) digitalWrite (Fans, HIGH);
+    if (!DEBUG) printOutput("Starting Vent: ");
+    else cout << "fake vent on\n";
 
-  digitalWrite (Fans,  LOW);
-  printOutput("Vent Off: ");
-  waitTime(fanRestartIntervalSec,fanRestartTimerInd);
+    waitTime(fanTimeInd,fanRestartTimerInd);
+
+    if (!DEBUG) digitalWrite (Fans,  LOW);
+    if (!DEBUG) printOutput("Vent Off: ");
+    else cout << "fake vent off\n";
+
+    waitTime(fanRestartIntervalSec,fanRestartTimerInd);
  }
  return NULL;
 }
@@ -124,12 +142,16 @@ void* lightTimer(void* arg){
   while(true){
     ReadTimeingsFile();
 
-    digitalWrite (Lights,  HIGH);
-    printOutput("Lights On: ");
+    if (!DEBUG)digitalWrite (Lights,  HIGH);
+    if (!DEBUG)printOutput("Lights On: ");
+    else cout << "fake light on\n";
+
     waitTime(lightRestartIntervalSec,lightRestartTimerInd);
 
-    digitalWrite (Lights, LOW);
-    printOutput("Lights Off: ");
+    if (!DEBUG)digitalWrite (Lights, LOW);
+    if (!DEBUG)printOutput("Lights Off: ");
+    else cout << "fake light on\n";
+
     waitTime(lightTimeInd,lightRestartTimerInd);
 
   }
@@ -140,26 +162,27 @@ void signal_callback_handler(int signum) {
    if(DEBUG)cout << "Signal Caught" << signum << endl;
 
    sem_destroy(&Signal1);
-   digitalWrite (Lights,  LOW);
-   digitalWrite (Water,  LOW);
-   digitalWrite (Fans,  LOW);
+   if (!DEBUG)digitalWrite (Lights,  LOW);
+   if (!DEBUG)digitalWrite (Water,  LOW);
+   if (!DEBUG)digitalWrite (Fans,  LOW);
    if(DEBUG)cout << "Killed Program\n";
    exit(signum);
 }
 
 int main (void)
 {
-  wiringPiSetup();
-  pinMode (Water, OUTPUT) ;
-  pinMode (Fans, OUTPUT) ;
-  pinMode (Lights, OUTPUT) ;
+  if (!DEBUG)wiringPiSetup();
+  if (!DEBUG)pinMode (Water, OUTPUT) ;
+  if (!DEBUG)pinMode (Fans, OUTPUT) ;
+  if (!DEBUG)pinMode (Lights, OUTPUT) ;
 
-  digitalWrite (Lights,  LOW);
-  digitalWrite (Water,  LOW);
-  digitalWrite (Fans,  LOW);
+  if (!DEBUG)digitalWrite (Lights,  LOW);
+  if (!DEBUG)digitalWrite (Water,  LOW);
+  if (!DEBUG)digitalWrite (Fans,  LOW);
   signal(SIGINT, signal_callback_handler);
-  delay(10000);
-  digitalWrite (Lights,  HIGH);
+  if (!DEBUG)delay(10000);
+  else cout << "skipping line \n";
+  if (!DEBUG)digitalWrite (Lights,  HIGH);
 
   sem_init(&Signal1, 0, 1);
   time_t now = time(0);
@@ -175,11 +198,11 @@ int main (void)
 
   sem_destroy(&Signal1);
 
-  digitalWrite (Lights,  LOW);
-  digitalWrite (Water,  LOW);
-  digitalWrite (Fans,  LOW);
+  if (!DEBUG)digitalWrite (Lights,  LOW);
+  if (!DEBUG)digitalWrite (Water,  LOW);
+  if (!DEBUG)digitalWrite (Fans,  LOW);
 
-  if(DEBUG)cout << "Killed Program\n";
+  if(DEBUG)cout << "Killed Program never should happen\n";
 
   return 0 ;
 }
